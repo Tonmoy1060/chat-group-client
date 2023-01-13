fd0c50f90c6f3633ec2c94e33dacb1e94fe002dc
Tonmoy1060 <abunaimtonmoy60@gmail.com>
2023-01-13 15:27:00 +0600

    header-section-set

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,22 @@
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
-  theme: {
-    extend: {},
+  content: ["./src/**/*.{html,js}"],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#f39c12",
+          secondary: "#FF6A3D",
+          accent: "#3c1f0a",
+          neutral: "#3d4451",
+          info: "#121a1d",
+          "base-100": "#ffffff",
+        },
+      },
+      "dark",
+      "cupcake",
+    ],
   },
-  plugins: [],
+  plugins: [require("daisyui")],
 }
